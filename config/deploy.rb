ce4f74d9041d8757269d835bb007d3da56aedcb7
James Adam <james@lazyatom.com>
2011-09-01 00:43:50 +0100

    Deploy the crontab to a shared user.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,8 @@ set :repository,  "git@github.com:freerange/harmonia.git"
 
 set :whenever_command, "bundle exec whenever"
 set(:whenever_identifier)   { application }
-set(:whenever_update_flags) { "--update-crontab #{whenever_identifier} -s password=#{password}" }
-set(:whenever_clear_flags)  { "--clear-crontab #{whenever_identifier}" }
+set(:whenever_update_flags) { "--update-crontab #{whenever_identifier} -u #{application_user} -s password=#{password}" }
+set(:whenever_clear_flags)  { "--clear-crontab #{whenever_identifier} -u #{application_user}" }
 
 before "deploy:update_code", "whenever:clear_crontab"
 after "deploy:update_code", "whenever:update_crontab"
@@ -17,11 +17,11 @@ after "deploy:rollback", "whenever:update_crontab"
 namespace :whenever do
   desc "Update application's crontab entries using Whenever"
   task :update_crontab, :roles => :app do
-    run "cd #{deploy_to} && #{whenever_command} #{whenever_update_flags}"
+    run "cd #{deploy_to} && sudo #{whenever_command} #{whenever_update_flags}"
   end
 
   desc "Clear application's crontab entries using Whenever"
   task :clear_crontab, :roles => :app do
-    run "cd #{deploy_to} && #{whenever_command} #{whenever_clear_flags}"
+    run "cd #{deploy_to} && sudo #{whenever_command} #{whenever_clear_flags}"
   end
 end
