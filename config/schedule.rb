faa4a02ca9d2af36cdffe920db0a94034a4ac314
Chris Roos <chris@seagul.co.uk>
2012-08-20 17:42:24 +0100

    Improve the instructions for the test_harmonia task
    This should make it easier for people to use this test task, should they
    wish.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,12 @@ harmonia_run = %{cd :path && bundle exec :ruby -I:path/lib -rharmonia -e}
 
 job_type :harmonia, %{#{harmonia_run} "Harmonia.new.:task(*:tasks)"}
 
+# Test task that we can use to test that the full stack is working correctly
+# See harmonia_test.rb for more information
+# every 3.minutes do
+#   harmonia :assign, tasks: [:harmonia_test]
+# end
+
 every :monday, :at => "11.50am" do
   harmonia :unassign, tasks: [:annual_return, :invoices, :weeknotes, :vat_return, :corporation_tax_payment, :corporation_tax_submission, :gardener, :fire_logbook, :wages, :drinks]
 end
--- lib/harmonia/mail/harmonia_test.rb
+++ lib/harmonia/mail/harmonia_test.rb
@@ -11,6 +11,20 @@ class Harmonia
 This is a test email that you can use to test that everything's hooked up properly in your deployment.
 
 For info only, the assignee of this 'task' is #{selected_person}.
+
+## Using this test
+
+* As the Harmonia user on the server, enable the code in schedule.rb that runs this task every 3 minutes.
+
+* From your development machine, run `cap whenever:update_crontab`.
+
+* Observe that you receive the test email every 3 minutes.
+
+* As the Harmonia user on the server, disable the code in schedule.rb that runs this task every 3 minutes.  A `git checkout .` should do it.
+
+* From your development machine, run `cap whenever:update_crontab`.
+
+* Finally, as the Harmonia user on the server remove any assignment for this task by running `cd /home/harmonia/app && ruby -Ilib -rharmonia -e "Harmonia.new.unassign(:harmonia_test)`.
               EOF
         end
       end
